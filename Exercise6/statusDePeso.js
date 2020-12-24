d7b067a3f6496d05291b4f1da38cae892bd6b8ff
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-24 15:00:00 -0300

    Correction in Exercise6

--- Exercise6/statusDePeso.js
+++ Exercise6/statusDePeso.js
@@ -1,15 +1,17 @@
 function statusDePeso(imc){
-  if(imc>0 && imc<18.5){
+  if(imc<0){
+    return 'IMC inválido!'
+  }if(imc<18.5){
     return 'Abaixo do peso!'
-  }if(imc>=18.5 && imc<25){
+  }if(imc<25){
     return 'Peso ideal!'
-  }if(imc>=25 && imc<30){
+  }if(imc<30){
     return 'Sobrepeso!'
-  }if(imc>=30 && imc<40){
+  }if(imc<40){
     return 'Obesidade!'  
   }if(imc>=40){
     return 'Obesidade Mórbida!'
-  }else return 'IMC inválido!'
+  }
 }
 
 module.exports = statusDePeso
